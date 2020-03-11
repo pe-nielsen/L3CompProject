f241b164e8f56be7402b3bb176643392936c37bb
Petur Einarsson Nielsen <peteynie@gmail.com>
2020-03-11 18:46:45 +0000

    initialise first long run on isotherms

--- resultsNCC/debugRun1/initial_parameters.py
+++ resultsNCC/debugRun1/initial_parameters.py
@@ -1,11 +1,11 @@
-simResultDir = r'resultsNCC/longRun2'
+simResultDir = r'resultsNCC/debugRun1'
 
 # densities = [3, 4, 5, 5.25, 5.5, 5.75, 6]
 # redTemps = [1, 11]
-densities = [1, 2, 3, 3.5, 4, 4.5, 5, 5.2, 5.4, 5.6, 5.8, 6, 6.2]
-redTemps = [1, 5, 10]
+densities = [3, 3.5, 4, 4.5, 5, 5.25, 5.5, 5.75, 6, 6.25]
+redTemps = [5]
 
-numPools = 39
+numPools = 10
 
 numParts = 10**3
 dim = 3
@@ -15,8 +15,8 @@ sigma = 3.4e-10
 well_depth = 1.65e-21
 Boltzmann_const = 1.38064852e-23  # m^2.kg.s^-2.K-1
 
-numEquilMoves = int(2e7)
-numEvolveMoves = int(2e7)
+numEquilMoves = int(5e6)
+numEvolveMoves = int(2e6)
 
 numEquilIter = 1000
 initDisplacement = 0.1
