f7d596b53523fb18f24130a8d82972e3b3632250
Peter Nielsen <splb68@ncc1.clients.dur.ac.uk>
2020-03-13 23:32:01 +0000

    moving back results for small NIST parameter runs

--- resultsNCC/NIST_params_1/initial_parameters.py
+++ resultsNCC/NIST_params_1/initial_parameters.py
@@ -1,4 +1,4 @@
-simResultDir = r'resultsNCC/NIST_params_2'
+simResultDir = r'resultsNCC/NIST_params_1'
 # simResultDir = r'simulationResults/debugRun2'
 
 # densities = [3, 4, 5, 5.25, 5.5, 5.75, 6]
@@ -9,10 +9,10 @@ simResultDir = r'resultsNCC/NIST_params_2'
 # redTemps=[5]
 
 densities = [1e-3, 3e-3, 5e-3, 7e-3, 9e-3, 7.76e-1, 7.80e-1, 8.2e-1, 8.60e-1, 9e-1]
-redTemps = [0.85, 0.9]
+redTemps = [0.85]
 
 
-numPools = 20
+numPools = 10
 
 numParts = 10**3
 dim = 3
@@ -23,10 +23,10 @@ sigma = 3.4e-10
 well_depth = 1.65e-21
 Boltzmann_const = 1.38064852e-23  # m^2.kg.s^-2.K-1
 
-numEquilMoves = int(1e7)
-numEvolveMoves = int(1e7)
+numEquilMoves = int(5e6)
+numEvolveMoves = int(5e6)
 
 
 numEquilIter = 1000
 initDisplacement = 0.1
-configSampleRate = 2000
+configSampleRate = 1000
