72d4661b3a3a4120d034505087563470c35c9695
Peter Nielsen <splb68@ncc1.clients.dur.ac.uk>
2020-03-19 22:39:21 +0000

    dataCollection 5e7 moves

--- initial_parameters.py
+++ initial_parameters.py
@@ -4,24 +4,24 @@ simResultDir = r'resultsNCC/dataCollection2'
 # redTemps = [0.85, 0.9]
 
 #dataCollectionFullRange 0-1.5
-densities = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
-redTemps = [1, 2, 3, 5, 10]
-numPools = 30
+#densities = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
+#redTemps = [1, 2, 3, 5, 10]
+#numPools = 30
 
 #dataCollectionSmallDens 0-0.35
-# densities = [0.05, 0.15, 0.25, 0.35, 0.45]
-# redTemps = [1, 5, 10]
-# numPools = 15
+#densities = [0.05, 0.15, 0.25, 0.35, 0.45]
+#redTemps = [1, 5, 10]
+#numPools = 15
 
 #dataCollectionMidDens 0.35-0.75
-# densities = [0.55, 0.65, 0.675, 0.725, 0.75]
-# redTemps = [1, 5, 10]
-# numPools = 15
+#densities = [0.55, 0.65, 0.675, 0.725, 0.75]
+#redTemps = [1, 5, 10]
+#numPools = 15
 
 #dataCollectionHighDens 0.75-1
-# densities = [0.76, 0.77, 0.78, 0.79, 0.825, 0.85, 0.875, 0.925, 0.95, 0.975]
-# redTemps = [1, 5, 10]
-# numPools = 15
+densities = [0.76, 0.77, 0.78, 0.79, 0.825, 0.85, 0.875, 0.925, 0.95, 0.975]
+redTemps = [1, 5, 10]
+numPools = 15
 
 
 # numPools = 20
