273a9a3da8856c0f4ebeb9b6e30d9dce67ddcde3
Peter Nielsen <splb68@ncc1.clients.dur.ac.uk>
2020-03-25 09:50:30 +0000

    feed bac high dens low temp data

--- initial_parameters.py
+++ initial_parameters.py
@@ -1,6 +1,6 @@
 #simResultDir = r'resultsNCC/dataCollection_lowTemp2'
 
-simResultDir = r'resultsNCC/convergenceTest'
+simResultDir = r'resultsNCC/convergence'
 #densities = [1e-3, 3e-3, 5e-3, 7e-3, 9e-3, 7.76e-1, 7.80e-1, 8.2e-1, 8.60e-1, 9e-1]
 #redTemps = [0.85, 0.9]
 #numPools = 20
@@ -63,8 +63,8 @@ sigma = 3.4e-10
 well_depth = 1.65e-21
 Boltzmann_const = 1.38064852e-23  # m^2.kg.s^-2.K-1
 
-numEquilMoves = int(1e5)
-numEvolveMoves = int(1e5)
+numEquilMoves = int(2e7)
+numEvolveMoves = int(2e7)
 
 numEquilIter = 1000
 initDisplacement = 0.1
